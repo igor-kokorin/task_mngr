846fafce61c6b071be1999f6a8d6c9fa8ee9e8f2
Igor Kokorin <kokorin.fear333777@yandex.ru>
2021-01-17 13:16:31 +0700

    respond to errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::API
+  rescue_from 'ActiveRecord::RecordNotFound' do |e|
+    render status: 404, json: { error: "#{e.model} not found" }
+  end
+
+  rescue_from 'ActiveRecord::RecordInvalid' do |e|
+    render status: 400, json: { error: "#{e.record.model_name.name}: #{e.record.errors.map { |e| e.message }.join(',')}" }
+  end
+
+  rescue_from 'Exceptions::ApiError' do |e|
+    byebug
+    render status: e.status, json: { error: e.message }
+  end
 end
